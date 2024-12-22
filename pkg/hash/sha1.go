8ee8544a41f24791138d9016e97d2c0923db160f
ffx64 <dente.estraga0l@icloud.com>
2024-12-22 14:37:03 -0300

    docs: add godoc comments to hash/sha1.go for better clarity

--- pkg/hash/sha1.go
+++ pkg/hash/sha1.go
@@ -5,10 +5,20 @@ import (
 	"encoding/hex"
 )
 
-func TextToSha1(text string) (hash string) {
-	sha1 := sha1.New()
-	sha1.Write([]byte(text))
-	hash = hex.EncodeToString(sha1.Sum(nil))
+// GenerateSha1Hash generates the SHA-1 hash of the given text.
+//
+// Parameters:
+// - text: A string containing the text to be hashed.
+//
+// Returns:
+// - hash: A string containing the SHA-1 hash of the input text.
+//
+// The function creates a new SHA-1 hash instance, writes the provided text as a byte slice,
+// and then encodes the resulting hash into a hexadecimal string. The hash is returned as a string.
+func GenerateSha1Hash(text string) (hash string) {
+    sha1 := sha1.New()
+    sha1.Write([]byte(text))
+    hash = hex.EncodeToString(sha1.Sum(nil))
 
-	return
+    return
 }
--- pkg/validator/stealer.go
+++
@@ -1,8 +0,0 @@
-package validator
-
-import "regexp"
-
-func ValStealerLog(logdata string) bool {
-	re := regexp.MustCompile(`([a-zA-Z0-9+.-]+://[^:/\s]+(?:/[^:\s]*)?):([^:]+):([^\n]*)`)
-	return re.MatchString(logdata)
-}
