10484c4c44b209b4d3b58a8bfaa363feea99165a
ffx64 <dente.estraga0l@icloud.com>
2024-12-22 14:10:19 -0300

    feat(config): implement database configuration using yaml

--- cmd/main.go
+++ cmd/main.go
@@ -5,10 +5,16 @@ import (
 	"fmt"
 	"log"
 	"os"
+	"path/filepath"
+	"strings"
+	"time"
 
-	"github.com/sentielxx/xlog-process/internal/db"
-	"github.com/sentielxx/xlog-process/internal/process"
-	"github.com/sentielxx/xlog-process/runner"
+	"github.com/sentielxx/searchx-indexer/internal/config"
+	"github.com/sentielxx/searchx-indexer/internal/db"
+	"github.com/sentielxx/searchx-indexer/internal/runner"
+	"github.com/sentielxx/searchx-indexer/pkg/hash"
+	"github.com/sentielxx/searchx-indexer/pkg/utils"
+	"github.com/sentielxx/searchx-indexer/process"
 )
 
 func init() {
@@ -16,98 +22,58 @@ func init() {
 }
 
 func main() {
-	database := flag.String("database", "127.0.0.1", "")
-	databaseName := flag.String("database-name", "xlog", "")
-	databasePort := flag.String("database-port", "5432", "")
-	databaseUser := flag.String("database-user", "postgres", "")
-	databasePass := flag.String("database-pass", "postgres", "")
-	file := flag.String("file", "", "path to the log file.")
+	configx, err := config.LoadConfig("config.yaml")
+	if err != nil {
+		log.Fatalf("[!] the file 'config.yaml' was not found.\n")
+	}
+
+	filedir := flag.String("file", "", "path to the log file.")
 	description := flag.String("description", "not information", "optional description for the logins.")
 	source := flag.String("source", "not information", "source dataleak")
 
 	flag.Parse()
 
-	if *file == "" {
-		print("[!] you did not provide the path to the log file, use the '-file' flag.")
+	if *filedir == "" {
+		log.Fatal("[!] you did not provide the path to the log file, use the '-file' flag.")
 	}
 
-	if _, err := os.Stat(*file); os.IsNotExist(err) {
-		fmt.Printf("[!] the log file '%s' was not found.\n", *file)
-		os.Exit(1)
+	fileInfo, err := os.Stat(*filedir)
+	if os.IsNotExist(err) {
+		log.Fatalf("[!] the log file '%s' was not found.%e\n", *filedir, err)
 	}
 
-	databaseNew := db.Database{
-		Host:     *database,
-		Port:     *databasePort,
-		Username: *databaseUser,
-		Password: *databasePass,
-		DBName:   *databaseName,
-	}
+	database := db.NewDatabase(configx.Database.Port, configx.Database.Host, configx.Database.Username, configx.Database.Password, configx.Database.Dbname, configx.Database.Sslmode)
 
-	databaseCursor, err := databaseNew.NewConnection()
+	cursor, err := database.Connect()
 	if err != nil {
-		fmt.Printf("[!] the log file '%s' was not found.\n", *file)
-		os.Exit(1)
+		log.Fatalf("[!] %s\n", err.Error())
 	}
-	defer databaseCursor.CloseConnection()
+	defer database.Close()
 
-	err = process.StealerLogsProcessData(databaseCursor, *file, *description, *source)
+	file := db.ModelComboListFile{
+		Name:                  fileInfo.Name(),
+		Hash:                  hash.GenerateSha1Hash(fileInfo.Name() + fmt.Sprint(fileInfo.Size())),
+		Size:                  fileInfo.Size(),
+		Type:                  strings.ReplaceAll(filepath.Ext(*filedir), ".", ""),
+		Description:           *description,
+		Source:                *source,
+		CreatedAt:             fileInfo.ModTime().Format("2006-01-02"),
+		ProcessedEntriesCount: 0,
+		ProcessedAt:           time.Now().Format("2006-01-02"),
+	}
+
+	log.Println("file hash       : ", file.Hash)
+	log.Println("file name       : ", file.Name)
+	log.Println("file description: ", file.Description)
+	log.Println("file source     : ", file.Source)
+	log.Println("file type       : ", file.Type)
+	log.Println("file size       : ", utils.ConvertFileSize(file.Size))
+	log.Println("file created at : ", file.CreatedAt)
+	println()
+
+	err = process.ComboListProcessDataleak(cursor, &file)
 
 	if err != nil {
-		log.Printf("[!] process file error: %e", err)
-		os.Exit(1)
+		log.Fatalln(err)
 	}
 }
-
-/*
-func main() {
-	file := flag.String("file", "", "path to the log file.")
-	databaseFile := flag.String("database", "database/xlog.db", "path to the sqlite database.")
-	description := flag.String("description", "not information", "optional description for the logins.")
-	disableSecurity := flag.Bool("disable-security", false, "disable options for security.")
-	enableMemory := flag.Bool("enable-temp-memory", false, "")
-	blockSize := flag.Int64("block-size", 1000, "set block size to process.")
-
-	flag.Parse()
-
-	if *file == "" {
-		print("[!] you did not provide the path to the log file, use the '-file' flag.")
-	}
-
-	if _, err := os.Stat(*file); os.IsNotExist(err) {
-		fmt.Printf("[!] the log file '%s' was not found.\n", *file)
-		os.Exit(1)
-	}
-
-	conn, err := database.Connect(*databaseFile)
-	if err != nil {
-		fmt.Printf("[!] the log file '%s' was not found.\n", *file)
-		os.Exit(1)
-	}
-	defer conn.Close()
-
-	if *disableSecurity {
-		if err = database.DisableSecuritySettings(conn); err != nil {
-			print("[!] error disable security:", err, "\n")
-			os.Exit(1)
-		}
-	}
-
-	if *enableMemory {
-		if err = database.SetTempStoreMemorySpeed(conn); err != nil {
-			print("[!] error set temp memory:", err, "\n")
-			os.Exit(1)
-		}
-	}
-
-	if err = database.CredentialsCreateTable(conn); err != nil {
-		print("[!] error create table:", err, "\n")
-	}
-
-	linestotal, linesprocessed := process.ProcessData(conn, *file, *description, *blockSize)
-
-	println("[+] total lines:", linestotal)
-	println("[+] total lines processed:", linesprocessed)
-	println("[+] processing completed successfully!")
-}
-*/
--- go.mod
+++ go.mod
@@ -1,5 +1,8 @@
-module github.com/sentielxx/xlog-process
+module github.com/sentielxx/searchx-indexer
 
 go 1.23.4
 
-require github.com/lib/pq v1.10.9 // indirect
+require (
+	github.com/lib/pq v1.10.9 // indirect
+	gopkg.in/yaml.v2 v2.4.0 // indirect
+)
